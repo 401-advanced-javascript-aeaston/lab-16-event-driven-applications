89a176844736f13179e5bedec5561473cfde11b0
Adrienne <adrienneeaston@gmail.com>
2019-08-10 10:32:16 -0700

    some edits, still just a start

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const fs = require('fs');
+const files = require('./lib/files/js');
 
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    // if(err) { throw err; } make these event listeners
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      // if(err) { throw err; }
-      // console.log(`${file} saved`); make this a promise
-    });
-  });
-};
+// const alterFile = (file) => {
+//   fs.readFile( file, (err, data) => {
+//     // if(err) { throw err; } make these event listeners
+//     let text = data.toString().toUpperCase();
+//     fs.writeFile( file, Buffer.from(text), (err, data) => {
+//       // if(err) { throw err; }
+//       // console.log(`${file} saved`); make this a promise
+//     });
+//   });
+// };
 
 let file = process.argv.slice(2).shift();
-alterFile(file);
+files.alterFile(file);
 
 // code from demo
 
-const events = require('./events.js');
+// const events = require('./events.js');
 
-require('./logger.js');
-require('./danger.js');
+// require('./logger.js');
+// require('./danger.js');
 
-events.emit('create', {id:17, name: 'Electronics'});
-events.emit('delete', {id: 17});
-events.emit('update', {id: 17, name: 'Food'});
+// events.emit('create', {id:17, name: 'Electronics'});
+// events.emit('delete', {id: 17});
+// events.emit('update', {id: 17, name: 'Food'});
